88868f3db7a85024b6d124c6467df6240a450fde
suffiang96 <56616975+suffiang96@users.noreply.github.com>
2020-01-14 17:21:51 -0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
-var map = L.map('map').fitWorld();
+alert("This site requests your location in order to better serve your spatial needs. Your location data will not be shared or stored for any purposes.");
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v10',
+    id: 'mapbox/light-v10',
     accessToken: 'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazJqbzVveXoxMHB1M25waDNmajltdjBzIn0.Rvaq-B6WyZ-s64wKMEdL2Q',
     tileSize: 512,
     zoomOffset: -1,
-}).addTo(map);
+});
+
+var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/dark-v10',
+        accessToken: 'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazJqbzVveXoxMHB1M25waDNmajltdjBzIn0.Rvaq-B6WyZ-s64wKMEdL2Q',
+        tileSize: 512,
+        zoomOffset: -1,
+});
+
+var map = L.map('map', {layers:[light]}).fitWorld();
 
 //locationPopup
 function onLocationFound(e) {
@@ -16,8 +27,29 @@ function onLocationFound(e) {
     L.marker(e.latlng).addTo(map)  //this adds a marker at the lat and long returned by the locate function.
         .bindPopup("You are within " + Math.round(radius * 3.28084) + " feet from this point").openPopup(); //this binds a popup to the marker. The text of the popup is defined here as well. Note that we multiply the radius by 3.28084 to convert the radius from meters to feet and that we use Math.round to round the conversion to the nearest whole number.
 
-    L.circle(e.latlng, radius).addTo(map); //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
+        if (radius = 100) {
+              L.circle(e.latlng, radius, {color: 'green'}).addTo(map);
+          }
+          else{
+              L.circle(e.latlng, radius, {color: 'red'}).addTo(map);
+          }
+
+          var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
+          var sunrise = times.sunrise.getHours();
+          var sunset = times.sunset.getHours();
+
+
+          var currentTime = new Date().getHours();
+              if (sunrise < currentTime && currentTime < sunset){
+                map.removeLayer(dark);
+                map.addLayer(light);
+              }
+              else {
+                map.removeLayer(light);
+                map.addLayer(dark);
+              }
 }
+
 map.on('locationfound', onLocationFound); //this is the event listener
 
 //error message
@@ -28,3 +60,10 @@ map.on('locationerror', onLocationError);
 
 //locator
 map.locate({setView: true, maxZoom: 16});
+
+var baseMaps = {
+    "Light": light,
+    "Dark": dark
+};
+
+L.control.layers(baseMaps).addTo(map);
